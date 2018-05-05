7847cc0f64871dfdaf951cf885f0705d2eaab9aa
evantoh <evanmwenda@gmail.com>
2018-05-05 17:30:43 +0300

    register a service in app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HeroService} from './hero.service';
 
 
 import { AppComponent } from './app.component';
@@ -18,7 +19,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
