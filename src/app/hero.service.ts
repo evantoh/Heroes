6542ae57c70d9630dcee7abb1343aa436ab353f9
evantoh <evanmwenda@gmail.com>
2018-05-07 15:08:51 +0300

    bind message to template

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,6 +8,8 @@ import { MessageService } from './message.service';
 export class HeroService {
   constructor(private messageService: MessageService) { }
   getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
   
