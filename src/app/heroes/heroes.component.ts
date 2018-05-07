18ea05bd5fb50da019e824bcc162e571b9bc0900
evantoh <evanmwenda@gmail.com>
2018-05-07 14:28:31 +0300

    calling getheroes method on ngOnInit lifecyle hook

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({  //decorator function that specifies the Angular metadata for the component.
   selector: 'app-heroes',//the component's CSS element selector
@@ -9,7 +9,7 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   // 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
@@ -17,9 +17,14 @@ export class HeroesComponent implements OnInit {
   this.selectedHero = hero;
 }
 
-  constructor() { }
+constructor(private heroService: HeroService) { }
 
+getHeroes(): void {
+  this.heroes = this.heroService.getHeroes();
+}
   ngOnInit() {
+    // this.getHeroes();
   }
+
 
 }
