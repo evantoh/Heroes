8ffb176f9961e640fa81a676480741e2a4c19a01
evantoh <evanmwenda@gmail.com>
2018-05-07 17:01:43 +0300

    add the dashboard to the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,8 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
 
 const routes: Routes=[
-  {
-    path:'heroes', component: HeroesComponent
-  }
+  { path:'heroes', component: HeroesComponent},
+  { path:'dashboard', component: DashboardComponent}
 ];
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
