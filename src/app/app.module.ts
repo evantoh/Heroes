8e991d675fbcd3aefe091d7c98463c4c7fd945bd
evantoh <evanmwenda@gmail.com>
2018-05-07 15:46:44 +0300

    register message service in app.module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
+
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+
 
 @NgModule({
   declarations: [
@@ -17,9 +22,7 @@ import { MessagesComponent } from './messages/messages.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [
-    // no need to place any providers due to the `providedIn` flag...
-  ],
-  bootstrap: [ AppComponent ]
+  providers: [HeroService,MessageService],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
